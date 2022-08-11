6ecbf06735638f652bbacf716d9ed5716e489651
Paul <paulotto.bethge@gmail.com>
2022-08-11 17:42:15 +0200

    Initial Commit

---
+++ Counterstike Quiz App 2.0 (IOS-16)/Counterstike_Quiz_App_2_0__IOS_16_App.swift
@@ -0,0 +1,17 @@
+//
+//  Counterstike_Quiz_App_2_0__IOS_16_App.swift
+//  Counterstike Quiz App 2.0 (IOS-16)
+//
+//  Created by Paul Bethge on 11.08.22.
+//
+
+import SwiftUI
+
+@main
+struct Counterstike_Quiz_App_2_0__IOS_16_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
